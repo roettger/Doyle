08319c64819cd5efb61c01f8649481578304166e
roettger <56430121+roettger@users.noreply.github.com>
2023-04-12 15:22:27 +0200

    Update extracting_distinctive_words_per_genre_tfidf.py

--- extracting_distinctive_words_per_genre_tfidf.py
+++ extracting_distinctive_words_per_genre_tfidf.py
@@ -1,4 +1,4 @@
-'''This code generates a visualization of the most distinctive words for four genres: detective, horror, adventure and historical.
+'''This code generates a visualization of the most distinctive words for four genres: detective, horror, adventure and historical.All novels are written by Arthur Conan Doyle.
 
 Here's how the code works:
 
@@ -7,7 +7,7 @@ Here's how the code works:
     The script iterates through the folders containing the text files for each genre, reading in each file and adding its text and genre label to the corpus list.
     The corpus list is converted into a pandas DataFrame.
     The text data in the DataFrame is preprocessed using the whitespace_nlp_with_sentences method from scattertext.
-    A scattertext Corpus object is created from the parsed texts in the DataFrame and the genre labels.
+    A scattertext corpus object is created from the parsed texts in the DataFrame and the genre labels.
     The top 10 distinctive words for each genre are extracted using tf-idf scoring.
     The distinctive words for each genre are printed to the console.
     A scattertext visualization is created using the produce_scattertext_explorer method. This method takes in several parameters, including the corpus, the category to focus on (in this case, 'detective'), the names for the category and non-category (i.e., 'Other Genres'), the minimum frequency for a term to be included, and the width of the visualization in pixels.
